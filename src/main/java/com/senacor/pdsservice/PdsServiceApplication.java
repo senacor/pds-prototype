984cd7339afc76ef140f5ab4a39162a2edf6721f
Salman Alhadziev <salman.alhadziev@senacor.com>
2024-01-16 10:56:46 +0100

    init commit

---
+++ src/main/java/com/senacor/pdsservice/PdsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.senacor.pdsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/senacor/pdsservice/PdsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senacor.pdsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
